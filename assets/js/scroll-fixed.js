acb12fa84c4ea1c910f3533a8a3129aab2402be5
Rob Sturcke <rs@rsvision.com>
2017-06-12 20:35:05 -0400

    button for simple push to top added

--- assets/js/scroll-fixed.js
+++ assets/js/scroll-fixed.js
@@ -1,8 +1,16 @@
 $(document).scroll(function() {
   let y = $(this).scrollTop();
+  let pushTop = $(this).scrollTop();
+
   if (y > 100) {
     $('.logo').fadeIn();
   } else {
     $('.logo').fadeOut();
-  }
+  };
+
+  if (pushTop > 700) {
+    $('.push-up').fadeIn();
+  } else {
+    $('.push-up').fadeOut();
+  };
 });
